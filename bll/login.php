b61d2d5803bba16da1955b0d2c0c24a20bc7f22f
Pragatheeswaran07 <pragatheeswaran2906@gamil.com>
2025-05-28 23:31:47 +0530

    commit all

---
+++ bll/login.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__ . '../../dmpr/dmlogin.php';
+require_once __DIR__ . '../../conn/db.php';
+class login
+{
+
+    public function __construtor()
+    {
+
+    }
+
+    public function fillloginData($Dataobject)
+    {
+
+
+
+
+    }
+
+
+}
+
+
+
+?>
